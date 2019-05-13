fefd3349912112de618891ecf78f3ecffb0313cc
Wes <wesmarberry@gmail.com>
2019-05-13 10:14:16 -0500

    log out button functioning and loggin user out

--- night-out/src/UserContainer/index.js
+++ night-out/src/UserContainer/index.js
@@ -77,6 +77,33 @@ deleteUser = async () => {
     }
 }
   
+  logout = async () => {
+    try {
+
+      const response = await fetch('http://localhost:3679/api/v1/user/logout', {
+        method: 'GET',
+        credentials: 'include', // on every request we have to send the cookie
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      const parsedResponse = await response.json();
+      console.log(parsedResponse);
+
+
+
+
+      
+
+      this.props.resetToLogin()
+
+
+
+    } catch (err) {
+
+    }
+  }
 
   render() {
 
@@ -87,6 +114,7 @@ deleteUser = async () => {
           <h1>{this.props.username} container displaying</h1>
           <h2>Your Previous Activities</h2>
           <button type="submit" onClick={this.deleteUser}>Delete Account</button>
+          <button type="submit" onClick={this.logout}>Log Out</button>
 
 
         </div>
