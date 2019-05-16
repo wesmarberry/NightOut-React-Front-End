6874814ab4d744e5742566d28089d9295314decf
Wes <wesmarberry@gmail.com>
2019-05-15 21:31:53 -0500

    trying to get user location

--- src/Login/index.js
+++ src/Login/index.js
@@ -104,6 +104,7 @@ class Login extends Component {
             <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange}/><br/>
             {display}
           </form>
+          <p>{this.state.lat}</p>
           <button onClick={this.props.showRegister}>Register</button>
         </div>
       )
